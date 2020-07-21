64f5047f2c757f46a41b3d5c701c91737d251724
= <=>
2020-07-21 18:43:09 +0800

    a

--- order_service_01/src/main/java/com/cagayake/controller/OrderController.java
+++ order_service_01/src/main/java/com/cagayake/controller/OrderController.java
@@ -1,12 +1,28 @@
 package com.cagayake.controller;
 
+import com.cagayake.feign.UserServiceFeignClient;
+import com.cagayake.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
 
-    public void addOrder(){};
+    private UserServiceFeignClient userServiceFeignClient;
 
+    @RequestMapping("/get")
+    public List<User> getAllUser(){
+       return userServiceFeignClient.findAllUser();
+    };
+
+
+    @Autowired
+    public void setUserServiceFeignClient(UserServiceFeignClient userServiceFeignClient) {
+        this.userServiceFeignClient = userServiceFeignClient;
+    }
 }
---
+++ order_service_01/src/main/java/com/cagayake/feign/UserServiceFeignClient.java
@@ -0,0 +1,15 @@
+package com.cagayake.feign;
+
+import com.cagayake.pojo.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@FeignClient(name = "user_service")
+public interface UserServiceFeignClient {
+
+    @RequestMapping(value = "/api/user/getAllUser" ,method = RequestMethod.GET)
+    public List<User> findAllUser();
+}
